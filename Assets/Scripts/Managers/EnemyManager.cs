025ce8f98d0d477c22cbcae46d9aba1eb9cce83a
Muhammad Hammad <mhnaeem@mun.ca>
2019-10-13 02:25:47 -0230

    random spawn points now on 50 points, time between bombs and shotgun is fixed, and alarm clock sound added

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -7,9 +7,11 @@ public class EnemyManager : MonoBehaviour
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
+    // Code by Muhammad Hammad
+    //This is a trigger that can be set to true anywhere in the program, once true it will change the random spawn point
     static bool makeNewSpawnPoint = false;
-
-    static int x = 0;
+    private float seconds = 20f;
+    private float timer = 0f;
 
 
     void Start ()
@@ -17,48 +19,74 @@ public class EnemyManager : MonoBehaviour
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
     }
 
+    // Code by Muhammad Hammad
     private void Update()
     {
-        // Change makeNewSpawnPoint to true anywhere in the scripts and it will make a new spawn point
-        if(x < 2)
+        timer += Time.deltaTime;
+
+        // If the more than 20seconds have passed inbetween the last random location change and the score is divisible by 50 then
+        if(timer >= seconds && ScoreManager.score != 0 && ScoreManager.score % 50 == 0)
         {
+            
+            timer = 0;
+            
+            //Set the trigger to true
             makeNewSpawnPoint = true;
-            if (x == 1)
+
+            //Play the alarm clock sound, indicating the change of spawn points
+            gameObject.GetComponent<AudioSource>().Play();
+
+            //If there are more than 3 original spawn points then delete the last one
+            if (GameObject.Find("EnemyManager").GetComponents<EnemyManager>().Length > 3)
             {
-                // This will delete the last created spawn point
                 deleteRandomSpawnPoint();
             }
-            x++;
         }
 
-        if (ScoreManager.score == 0 && makeNewSpawnPoint)
+        //If trigger is true then create a new spawn point and trigger goes back to false
+        if (makeNewSpawnPoint)
         {
             makeNewRandomSpawnPoint();
+            makeNewSpawnPoint = false;
         }
     }
 
+    // Deletes the the last random spawn point
     public static void deleteRandomSpawnPoint()
     {
+        // Find the enemy manager object then get all it's EnemyManager components and destroy the last one (random one).
         GameObject enemyManagerObject = GameObject.Find("EnemyManager");
         EnemyManager[] enemyManagers = enemyManagerObject.GetComponents<EnemyManager>();
-        Destroy(enemyManagers[enemyManagers.Length - 1]);
+        DestroyImmediate(enemyManagers[enemyManagers.Length - 1]);
         
     }
 
+    //Creates a new random spawn point
     public void makeNewRandomSpawnPoint()
     {
-        makeNewSpawnPoint = false;
+        // Find the enemy manager object
         GameObject enemyManagerObject = GameObject.Find("EnemyManager");
+
+        // Add a new EnemyManager script to the object for a random spawnpoint
         enemyManagerObject.AddComponent<EnemyManager>();
+
+        // Array of all the EnemyManager components
         EnemyManager[] enemyManagers = enemyManagerObject.GetComponents<EnemyManager>();
 
+        // Get the last last EnemyManager script which is the new one just added
         EnemyManager newEnemyManager = enemyManagers[enemyManagers.Length - 1];
+
+        // Initialize the spawn point position in the newly added random EnemyManager script
         newEnemyManager.spawnPoints = new Transform[1];
         newEnemyManager.spawnPoints[0] = GameObject.Find("RandomSpawnPoint").GetComponent<Transform>();
 
+        // Select a random coordinate for the new spawn point
         newEnemyManager.spawnPoints[0].position = new Vector3(Random.Range(-25, 26), 0, Random.Range(-25, 26));
         
+        // Load Zombunny as the type of spawn enemy
         newEnemyManager.enemy = Resources.Load("Zombunny") as GameObject;
+
+        // Set the player health and spawn intervals
         newEnemyManager.playerHealth = playerHealth;
         newEnemyManager.spawnTime = 15f;
     }
--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -16,22 +16,23 @@ public class PlayerShooting : MonoBehaviour
     AudioSource gunAudio;
     Light gunLight;
     float effectsDisplayTime = 0.2f;
-    bool specialWeapon = false;
+    bool specialWeapon = false;         //Weapon for the frenemy
 
+    //Code by Muhammad Hammad
 
     //Code for grenade launcher
-    public static bool grenadeLauncher = false;
-    float timeBetweenBombs = 0.5f;
+    public static bool grenadeLauncher = false; //Switch to make grenade launcher available
+    float timeBetweenBombs = 30f;
     float rangeGrenade = 8f;
     int damagePerBomb = 50;
-    bool grenadeLauncherBeingUsed = false;
+    bool grenadeLauncherBeingUsed = false;      //Trigger to identify whether the grenade launcher is being used
 
     //Code for shotgun
-    public static bool shotgun = false;
-    float timeBetweenShotGun = 0.7f;
+    public static bool shotgun = false; //Switch to make shotgun available
+    float timeBetweenShotGun = 10f;
     float rangeShotGun = 3f;
     int damagePerShotGun = 35;
-    bool shotgunBeingUsed = false;
+    bool shotgunBeingUsed = false;    //Trigger to identify whether the shotgun is being used
 
 
     //Code for playing gun particles
@@ -47,6 +48,7 @@ public class PlayerShooting : MonoBehaviour
         gunAudio = GetComponent<AudioSource>();
         gunLight = GetComponent<Light>();
 
+        // These are effects for shotgun and grenade launcher, modification of the hit particles
         explosiveEffects = GameObject.Find("ExplParticle");
         shotgunEffects = GameObject.Find("ShotgunParticle");
     }
@@ -56,16 +58,19 @@ public class PlayerShooting : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        if(ScoreManager.score % 50 == 0 && timer * 0.1 >= timeBetweenBombs && Time.timeScale != 0 && ScoreManager.score != 0)
+        // If the score is divisible by 50 and time between bombs has passed then make grenade launcher available
+        if(ScoreManager.score % 50 == 0 && timer >= timeBetweenBombs && Time.timeScale != 0 && ScoreManager.score != 0)
         {
             grenadeLauncher = true;
         }
 
-        if (timer * 0.3 >= timeBetweenShotGun && Time.timeScale != 0 && ScoreManager.score != 0 && ScoreManager.score % 20 == 0)
+        // If the score is divisible by 20 and time between shotgun has passed then make shotgun available
+        if (timer >= timeBetweenShotGun && Time.timeScale != 0 && ScoreManager.score != 0 && ScoreManager.score % 20 == 0)
         {
             shotgun = true;
         }
 
+        //If left shift is pressed and shotgun is available
         if (shotgun && Input.GetButton("Fire3"))
         {
             shotgun = false;
@@ -74,6 +79,7 @@ public class PlayerShooting : MonoBehaviour
             shotgunBeingUsed = false;
         }
 
+        //If Spacebar is pressed and grenade launcher is available
         if (Input.GetButton("Jump") && grenadeLauncher)
         {
             grenadeLauncher = false;
@@ -102,6 +108,7 @@ public class PlayerShooting : MonoBehaviour
         gunLight.enabled = false;
     }
 
+    // Changes the colour of the gunline renderer based on the type of weapon being used
     void changeGunLineColour()
     {
         if (shotgunBeingUsed)
@@ -122,10 +129,11 @@ public class PlayerShooting : MonoBehaviour
         }
     }
 
+    // Returns the correct range of the gun based on the type of the weapon being used
     float rangeSelect()
     {
         if (grenadeLauncherBeingUsed)
-        {
+        { 
             return rangeGrenade;
         }
         else if (shotgunBeingUsed)
@@ -138,6 +146,7 @@ public class PlayerShooting : MonoBehaviour
         }
     }
 
+    // Edited by Muhammad Hammad, some of it is from the original survival shooter game
     void Shoot()
     {
         timer = 0f;
@@ -152,18 +161,21 @@ public class PlayerShooting : MonoBehaviour
         gunLine.enabled = true;
         gunLine.SetPosition(0, transform.position);
 
+        // Change the colour of the gun line
         changeGunLineColour();
 
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
 
+        //Select the correct range based on the weapon
         float tempRange = rangeSelect();
 
+        //Raycast and see what collider it hits
         if (Physics.Raycast(shootRay, out shootHit, tempRange, shootableMask))
         {
+            // Play the effects based on what type of weapon is being used
             if (grenadeLauncherBeingUsed)
             {
-                //Code for explosive effects
                 explosiveEffects.transform.position = shootHit.transform.position;
                 explosiveEffects.GetComponent<ParticleSystem>().Play();
             }
@@ -173,16 +185,20 @@ public class PlayerShooting : MonoBehaviour
                 shotgunEffects.GetComponent<ParticleSystem>().Play();
             }
 
+            // Get the EnemyHealth script from the collider that is hit
             EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
 
+            //If an enemy was hit then proceed otherwise go to else
             if (enemyHealth != null)
             {
                 if (grenadeLauncherBeingUsed || shotgunBeingUsed)
                 {
                     Vector3 enemyHitPos = enemyHealth.transform.position;
 
+                    //Get all the colliders in the radius of 5f
                     foreach (Collider i in Physics.OverlapSphere(enemyHitPos, 5f))
                     {
+                        //If the collider is an enemy then deal damage depending on what weapon was used.
                         if (i.GetComponent<EnemyHealth>() != null)
                         {
                             if (grenadeLauncherBeingUsed)
@@ -208,8 +224,12 @@ public class PlayerShooting : MonoBehaviour
             gunLine.SetPosition(1, shootHit.point);
 
         }
+
+        //If no colliders were hit by the raycast then
         else
         {
+            //Play the effects for each weapon and change the gunline distance
+
             if (grenadeLauncherBeingUsed)
             {
                 //Code for explosive effects
