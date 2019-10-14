527d873deb43efc819240d7a9e069ad472a67eb7
Muhammad Hammad <mhnaeem@mun.ca>
2019-10-14 02:44:03 -0230

    popup manager and tutorial manager added - written by RonakBParmar

---
+++ Assets/Scripts/Managers/PopUpManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int health;
+    public GameObject healthPopup;
+    public Text myText;
+    public float start, end;
+    public Color newColor;
+
+    Text text;
+
+    void Awake()
+    {
+        healthPopup = GameObject.Find("HealthPopup");
+        text = healthPopup.GetComponent<Text>();
+        healthPopup.SetActive(false);
+        health = 100;
+        start = 0f;
+        end = 2f;
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Wachout! Your health is: " + health;
+        if (health == 50)
+        {
+            healthPopup.SetActive(true);
+            start += Time.deltaTime;
+            
+            if (start > end)
+            {
+                healthPopup.SetActive(false);
+            }
+        }
+        if (health == 20)
+        {
+            healthPopup.SetActive(true);
+            text.color = Color.Lerp(text.color, newColor, Time.deltaTime);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popupIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popupIndex = 0;
+        popUps[0].SetActive(true);
+        for (int i = 1; i < popUps.Length; i++)
+        {
+            popUps[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (popupIndex == 0)
+        {
+            if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
+                popUps[popupIndex].SetActive(false);
+                popupIndex++;
+            }
+        }
+        else  if(popupIndex == 1) {
+            popUps[popupIndex].SetActive(true);
+            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) //add the arrow keys or statement
+            {
+                popUps[popupIndex].SetActive(false);
+                popupIndex++;
+            }
+        }
+
+        else if (popupIndex == 2)
+        {
+            popUps[popupIndex].SetActive(true);
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                popUps[popupIndex].SetActive(false);
+                popupIndex++;
+            }
+        }
+
+        else if (popupIndex == 3)
+        {
+            popUps[popupIndex].SetActive(true);
+            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Space))
+            {  
+                popUps[popupIndex].SetActive(false);
+                popupIndex++;
+            }
+        }
+
+
+    }
+}
