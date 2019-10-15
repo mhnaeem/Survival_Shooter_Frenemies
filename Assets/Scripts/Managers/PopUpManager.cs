db8763309caec843209ded518ac447b4d16d3a70
Muhammad Hammad <mhnaeem@mun.ca>
2019-10-15 01:26:18 -0230

    added the shotgun and grenade launcher improvements - written by RonakBParmar

--- Assets/Scripts/Managers/PopUpManager.cs
+++ Assets/Scripts/Managers/PopUpManager.cs
@@ -6,46 +6,48 @@ using UnityEngine.UI;
 public class PopUpManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static int health;
+    //public static int health;
     public GameObject healthPopup;
-    public Text myText;
     public float start, end;
-    public Color newColor;
-
+    public Color noAplha;
+    PlayerHealth health;
     Text text;
 
     void Awake()
     {
+        health = GameObject.Find("Player").GetComponent<PlayerHealth>();
         healthPopup = GameObject.Find("HealthPopup");
         text = healthPopup.GetComponent<Text>();
         healthPopup.SetActive(false);
-        health = 100;
         start = 0f;
         end = 2f;
-        
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        text.text = "Wachout! Your health is: " + health;
-        if (health == 50)
+        text.text = "Wachout! Your health is less then: " + health.currentHealth;
+        if (health.currentHealth >= 40 && health.currentHealth <= 50)
         {
-            healthPopup.SetActive(true);
-            start += Time.deltaTime;
             
+            healthPopup.SetActive(true);
+
+            start += Time.deltaTime;
+
             if (start > end)
             {
                 healthPopup.SetActive(false);
             }
         }
-        if (health == 20)
+        if (health.currentHealth <= 20)
         {
+            text.text = "Wachout! Your health is less then: " + health.currentHealth;
             healthPopup.SetActive(true);
-            text.color = Color.Lerp(text.color, newColor, Time.deltaTime);
-
+            text.color = Color.Lerp(text.color, noAplha, Time.deltaTime);
         }
+
+       
     }
 
 }
--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -1,37 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
     private int popupIndex;
+    public float start, end, start1, end1, start2, end2;
+    public Color noAplha;
+    Text shotgunText;
+    Text gLText;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        popUps = new GameObject[5];
+        popUps[0] = GameObject.Find("moveLR");
+        popUps[1] = GameObject.Find("moveWS");
+        popUps[2] = GameObject.Find("shoot");
+        popUps[3] = GameObject.Find("SGActive");
+        popUps[4] = GameObject.Find("GLActive");
         popupIndex = 0;
-        popUps[0].SetActive(true);
         for (int i = 1; i < popUps.Length; i++)
         {
             popUps[i].SetActive(false);
         }
+        //turning the shotgun and gl transparent
+        popUps[3].GetComponent<CanvasRenderer>().SetAlpha(0f);
+        popUps[4].GetComponent<CanvasRenderer>().SetAlpha(0f);
+
+        start1 = 0f;
+        end1 = 6f;
+        start2 = 0f;
+        end2 = 6f;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (popupIndex <= 4) {
+            popUps[popupIndex].SetActive(true);
+        }
+
         if (popupIndex == 0)
         {
-            if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
                 popUps[popupIndex].SetActive(false);
                 popupIndex++;
             }
         }
-        else  if(popupIndex == 1) {
-            popUps[popupIndex].SetActive(true);
-            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) //add the arrow keys or statement
+        else if (popupIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) //add the arrow keys or statement
             {
                 popUps[popupIndex].SetActive(false);
                 popupIndex++;
@@ -40,7 +65,6 @@ public class TutorialManager : MonoBehaviour
 
         else if (popupIndex == 2)
         {
-            popUps[popupIndex].SetActive(true);
             if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.LeftControl))
             {
                 popUps[popupIndex].SetActive(false);
@@ -48,15 +72,43 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        else if (popupIndex == 3)
+        else if (popupIndex == 3 && PlayerShooting.shotgun)
         {
-            popUps[popupIndex].SetActive(true);
-            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Space))
-            {  
-                popUps[popupIndex].SetActive(false);
+            shotgunText = popUps[popupIndex].GetComponent<Text>();
+            popUps[popupIndex].GetComponent<CanvasRenderer>().SetAlpha(255f);
+            start1 += Time.deltaTime;
+            if (start1 >= end1)
+            {
+                popUps[popupIndex].GetComponent<CanvasRenderer>().SetAlpha(0f);
                 popupIndex++;
+
             }
+            if (start1 >= 2)
+            {
+                shotgunText.text = "Check the shotgun icon on the bottom-left" +"\n" +"next time when its available";
+            }
+            
+
         }
+
+        else if (popupIndex == 4 && PlayerShooting.grenadeLauncher)
+        {
+            gLText = popUps[popupIndex].GetComponent<Text>();
+            popUps[popupIndex].GetComponent<CanvasRenderer>().SetAlpha(255f);
+            start2 += Time.deltaTime;
+            if (start2 >= end2)
+            {
+                popUps[popupIndex].GetComponent<CanvasRenderer>().SetAlpha(0f);
+                popupIndex++;
+
+            }
+            if (start2 >= 2)
+            {
+                gLText.text = "Check the bomb icon on the bottom-left" + "\n" + "next time when its available";
+            }
+
+        }
+        
 
 
     }
